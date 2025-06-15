bd985a23fc1708d425f04766849130574d4b7380
Toxic <localgod13@gmail.com>
2025-06-15 14:41:04 -0700

    almost there2

--- websocket-server/server.ts
+++ websocket-server/server.ts
@@ -1,11 +1,14 @@
 import { WebSocket, WebSocketServer } from 'ws';
 import { createServer } from 'http';
+import { v4 as uuidv4 } from 'uuid';
 
 const server = createServer();
 const wss = new WebSocketServer({ server });
 
 wss.on('connection', (ws: WebSocket) => {
   console.log('Client connected');
+  const id = uuidv4().slice(0, 6);
+  ws.send(JSON.stringify({ type: 'room-code', code: id }));
 });
 
 const PORT = process.env.PORT || 3000;
